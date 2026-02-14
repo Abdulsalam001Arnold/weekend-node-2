429b728a6b915928920764dc9efae231b8a2335b
Abdulsalam001Arnold <abdulsalamabulhamidarnold@gmail.com>
2026-02-14 10:23:06 +0100

    code updated

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ await connectToDB()
 
 
 app.use(userRoutes)
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+
+if(process.env.NODE_ENV !== "production") {
+    app.listen(3000, () => {
+        console.log("Server running on port 3000")
+    })
+}
+
+export default app
